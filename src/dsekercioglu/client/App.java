a78dbc0eeca144f5f84afbe9387d0d0c0bffc59b
doruksekercioglu <>
2018-03-25 22:30:36 +0300

    JavaFX window to choose characters.

---
+++ src/dsekercioglu/client/App.java
@@ -0,0 +1,99 @@
+package dsekercioglu.client;
+
+import dsekercioglu.general.characters.Animal;
+import dsekercioglu.general.characters.Team;
+import dsekercioglu.simplifier.SimplifiedGUI;
+import java.io.File;
+import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Label ip_label = SimplifiedGUI.newLabel("Enter IP:");
+        TextField ip_textField = SimplifiedGUI.newTextField("", 150, true);
+
+        Label name_label = SimplifiedGUI.newLabel("Enter Name:");
+        TextField name_textField = SimplifiedGUI.newTextField("", 150, true);
+
+        Label team_label = SimplifiedGUI.newLabel("Select Team:");
+        ChoiceBox team_choiceBox = SimplifiedGUI.newChoiceBox(0, "RED", "GREEN", "BLUE", "YELLOW");
+
+        Node[][] gridNodes = {{ip_label, name_label, team_label},
+        {ip_textField, name_textField, team_choiceBox}};
+
+        GridPane gridPane = SimplifiedGUI.newGridPane(20, 10, 30, Color.ALICEBLUE, gridNodes);
+
+        ToggleGroup toggleGroup = new ToggleGroup();
+
+        Button btn = SimplifiedGUI.newButton("Run game", Color.GOLD, (a) -> {
+
+            String ip = ip_textField.getText();
+            String name = name_textField.getText();
+            int selectedIndex = team_choiceBox.getSelectionModel().getSelectedIndex();
+            Team team = Team.values()[selectedIndex];
+            Animal animal = Animal.values()[toggleGroup.getToggles().indexOf(toggleGroup.getSelectedToggle())];
+            WildNature.connect(ip, name, animal, team);
+
+        });
+
+        Label character_label = SimplifiedGUI.newLabel("Select Character:");
+
+        String[] fileNames = {"Marlin", "BlackMarlin", "Crocodile", "Shark",
+            "ElectricEel", "Barracuda", "MegaMouth", "Orca", "Hippo", "ColossalSquid",
+            "DoodFish", "ElectricMarlin", "Guardian"};
+        RadioButton[] chars = new RadioButton[fileNames.length];
+        for (int i = 0; i < fileNames.length; i++) {
+            chars[i] = SimplifiedGUI.newRadioButton("", true, toggleGroup);
+            File imageFile = new File("img/" + fileNames[i] + ".png");
+            Image image = new Image(imageFile.toURI().toString());
+            ImageView imageView = new ImageView(image);
+            imageView.setPreserveRatio(true);
+            imageView.setFitWidth(150);
+            chars[i].setGraphic(imageView);
+        }
+
+        int numOfRows = 3;
+        Node[][] characterNodes = new Node[numOfRows][(int) Math.ceil((double) fileNames.length / numOfRows)];
+        int k = 0;
+        for (int i = 0; i < numOfRows; i++) {
+            for (int j = 0; j < characterNodes[i].length; j++) {
+                if (k == fileNames.length) {
+                    break;
+                }
+                characterNodes[i][j] = chars[k];
+                k++;
+            }
+        }
+
+        GridPane characterPane = SimplifiedGUI.newGridPane(10, 10, 15, Color.ALICEBLUE, characterNodes);
+        Node[] nodes = {gridPane, character_label, characterPane, btn};
+        VBox root = SimplifiedGUI.newVBox(30, 30, Color.ALICEBLUE, nodes);
+
+        stage.setTitle("Wild Nature");
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.show();
+
+    }
+
+}
--- src/dsekercioglu/client/WildNature.java
+++ src/dsekercioglu/client/WildNature.java
@@ -19,33 +19,18 @@ import processing.core.PApplet;
 
 public class WildNature extends PApplet {
 
-    private Visualizer v;
+    private static Visualizer v;
 
     private static ArrayList<DrawInfo> characters = new ArrayList();
     private static Animal animal;
-    private static String name = null;
+    private static String name;
 
     public static Client client;
 
-    TextBox ipBox;
-    TextBox nameBox;
-    CharacterChooser cc;
-
-    String IP = null;
-    Animal character = null;
-    Team t = null;
-
     @Override
     public void setup() {
-//        name = "";
-//        for (int i = 0; i < 10; i++) {
-//            name += (int) (Math.random() * 10);
-//        }
-        Team[] teams = Team.values();
-        t = teams[(int) (Math.random() * teams.length)];
-        cc = new CharacterChooser(this);
-        ipBox = new TextBox(600, 100, true, this);
-        nameBox = new TextBox(600, 100, true, this);
+        v = new Visualizer(name, animal, this);
+        v.setImages();
         surface.setResizable(true);
     }
 
@@ -57,88 +42,13 @@ public class WildNature extends PApplet {
     @Override
     public void draw() {
         clear();
-        if (IP == null) {
-            fill(255);
-            textSize(20);
-            text("Enter server IP:", 0, 100);
-            ipBox.draw(0, 200);
-            if (keyPressed && key == ENTER && ipBox.getText().length() > 1) {
-                IP = ipBox.getText();
-            }
-        } else {
-            if (name == null) {
-                fill(255);
-                textSize(20);
-                text("Enter your name:", 0, 100);
-                nameBox.draw(0, 200);
-                if (keyPressed && key == ENTER && nameBox.getText().length() > 1) {
-                    name = nameBox.getText();
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(WildNature.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
-            } else {
-                if (animal == null) {
-                    if (cc.update()) {
-                        animal = cc.chosenCharacter;
-                        v = new Visualizer(name, animal, this);
-                        v.setImages();
-                        connect();
-                    }
-                } else {
-                    v.update(characters);
-                }
-            }
-        }
+        v.update(characters);
     }
-
-    public static void main(String[] args) {
-        PApplet.main(new String[]{WildNature.class.getName()});
-    }
-
-    public void setCharacter() {
+    
+    public static void connect(String ip, String name, Animal animal, Team team) {
         try {
-            Scanner scn = new Scanner(System.in);
-            System.out.println("Enter username:");
-            name = scn.nextLine();
-
-            while (true) {
-                System.out.println("Choose a character...");
-
-                Animal[] animals = Animal.values();
-                for (int i = 0; i < animals.length; i++) {
-                    System.out.println((i + 1) + ")" + animals[i].name().replaceAll("_", " "));
-                }
-
-                int characterNo = scn.nextInt();
-
-                if (characterNo >= 1 && characterNo <= Animal.values().length) {
-                    animal = animals[characterNo - 1];
-                    break;
-                }
-            }
-            Team team;
-            while (true) {
-                System.out.println("Choose your favorite color...");
-
-                Team[] teams = Team.values();
-                for (int i = 0; i < teams.length; i++) {
-                    System.out.println((i + 1) + ")" + teams[i].name());
-                }
-
-                int characterNo = scn.nextInt();
-
-                if (characterNo >= 1 && characterNo <= Animal.values().length) {
-                    team = teams[characterNo - 1];
-                    break;
-                }
-            }
-
-            System.out.println("Enter server IP address:");
-            String ip = scn.next();
-
+            WildNature.name = name;
+            WildNature.animal = animal;
             client = new Client();
             Kryo kryo = client.getKryo();
             kryo.register(ControlInfo.class);
@@ -148,7 +58,7 @@ public class WildNature extends PApplet {
             kryo.register(DrawInfo.class);
             kryo.register(Team.class);
             new Thread(client).start();
-            client.connect(5000, ip, 54555, 54777);
+            client.connect(5000, ip, 54551, 54772);
 
             PlayerInfo pi = new PlayerInfo();
             pi.character = name + "/" + animal.name();
@@ -162,36 +72,7 @@ public class WildNature extends PApplet {
                     }
                 }
             });
-        } catch (IOException ex) {
-            Logger.getLogger(WildNature.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    public void connect() {
-        try {
-            client = new Client();
-            Kryo kryo = client.getKryo();
-            kryo.register(ControlInfo.class);
-            kryo.register(CharacterInfo.class);
-            kryo.register(PlayerInfo.class);
-            kryo.register(ArrayList.class);
-            kryo.register(DrawInfo.class);
-            kryo.register(Team.class);
-            new Thread(client).start();
-            client.connect(5000, IP, 54551, 54772);
-
-            PlayerInfo pi = new PlayerInfo();
-            pi.character = name + "/" + animal.name();
-            pi.team = t;
-            client.sendTCP(pi);
-            client.addListener(new Listener() {
-                @Override
-                public void received(Connection connection, Object object) {
-                    if ((object instanceof CharacterInfo)) {
-                        WildNature.characters = ((CharacterInfo) object).characters;
-                    }
-                }
-            });
+            PApplet.main(new String[]{WildNature.class.getName()});
         } catch (IOException ex) {
             Logger.getLogger(WildNature.class.getName()).log(Level.SEVERE, null, ex);
         }
